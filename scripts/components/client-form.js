7828d30b8e6a19949df3f8c19b8ed2928c174579
Michiel Renty <m.renty@gmail.com>
2016-08-04 22:15:23 +0200

    Added support for localstorage

--- scripts/components/client-form.js
+++ scripts/components/client-form.js
@@ -6,16 +6,39 @@
 import React from 'react';
 
 var ClientForm = React.createClass({
-    updateClient: function (event) {
-        event.preventDefault();
+    getClient: function () {
         let client = {
             name: this.refs.name.value,
             address: this.refs.address.value,
             zipCode: this.refs.zipCode.value,
             city: this.refs.city.value,
         }
+        return client;
+    },
+    updateClient: function (event) {
+        event.preventDefault();
+        let client = this.getClient();
 
         this.props.updateClient(client);
+    },
+    saveClient: function (event) {
+        event.preventDefault();
+        let client = this.getClient();
+        localStorage.setItem('invoice-client', JSON.stringify(client));
+    },
+    loadClient: function (event) {
+        event.preventDefault();
+
+        if (localStorage.getItem('invoice-client') !== null) {
+            let client = JSON.parse(localStorage.getItem('invoice-client')) || {};
+
+            this.refs.name.value = client.name;
+            this.refs.address.value = client.address;
+            this.refs.zipCode.value = client.zipCode;
+            this.refs.city.value = client.city;
+
+            this.props.updateClient(client);
+        }
     },
     render: function () {
         return (
@@ -25,6 +48,8 @@ var ClientForm = React.createClass({
                 <input type="text" ref="zipCode" placeholder="Zip-code" />
                 <input type="text" ref="city" placeholder="City" />
                 <button type="submit">Update</button>
+                <button type="button" onClick={this.saveClient}>Save</button>
+                <button type="button" onClick={this.loadClient}>Get latest</button>
             </form>
         )
     }
