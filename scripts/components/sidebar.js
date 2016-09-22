ad092ad848bc4a4ea3f4a882991fdaf72de61312
Michiel Renty <m.renty@gmail.com>
2016-09-22 20:33:44 +0200

    Added support for printing to Google Drive
    You can now print directly to Google Drive as a PDF so you always have
    a backup.

--- scripts/components/sidebar.js
+++ scripts/components/sidebar.js
@@ -8,14 +8,77 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import AddTaskForm from './add-task-form';
 import MetaForm from './meta-form';
 import ClientForm from './client-form';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 var Sidebar = React.createClass({
     saveToDrive: function () {
+
+        if (!navigator.onLine) {
+            console.warn('No active internet connection!');
+            return false;
+        }
+
         const gadget = new cloudprint.Gadget();
-        gadget.setPrintDocument("url", this.props.meta.docnr, location);
-        gadget.openPrintDialog();
+        const name = this.props.meta.docnr;
+        const a4 = {
+            width: 595.28,
+            height: 841.89
+        };
+        const element = document.querySelector('.document');
+        const cache_width = element.style.width;
+
+        const pdf = new jsPDF('p', 'mm', 'a4');
+        const zoomFactor = 1.33333;
+
+        //RETINA
+        const originalWidth = element.offsetWidth;
+        const originalHeight = element.offsetHeight;
+        const scaledCanvas = document.createElement('canvas');
+        const scaledContext = scaledCanvas.getContext('2d');
+
+        document.body.classList.add('print-preview');
+        element.style.width = (a4.width * zoomFactor) + 'px';
+        element.style.height = (a4.height * zoomFactor) + 'px';
+
+        //RETINA
+        element.style.width = originalWidth + "px";
+        element.style.height = originalHeight + "px";
+        element.style.position = 'absolute';
+        element.style.top = '0';
+        element.style.left = '0';
+
+        scaledCanvas.width = originalWidth * 2;
+        scaledCanvas.height = originalHeight * 2;
+        scaledCanvas.style.width = originalWidth + 'px';
+        scaledCanvas.style.height = originalHeight + 'px';
+
+        scaledContext.webkitImageSmoothingEnabled = false;
+        scaledContext.mozImageSmoothingEnabled = false;
+        scaledContext.imageSmoothingEnabled = false;
+        scaledContext.scale(2, 2);
+
+        html2canvas(element, {
+            imageTimeout: 2000,
+            removeContainer: true,
+            canvas: scaledCanvas
+        }).then((canvas) => {
+            // Has to be JPEG since PNG crashes jsPDF
+            // https://github.com/MrRio/jsPDF/issues/702
+            var img = canvas.toDataURL('image/jpeg');
+            pdf.addImage(img, 'JPEG', 0, 0, pdf.internal.pageSize.width, pdf.internal.pageSize.height);
+            let rawData = pdf.output('datauristring').split(',')[1];
+
+            gadget.setPrintDocument('application/pdf', name, rawData, 'base64');
+            gadget.openPrintDialog();
+        });
+
+        element.style.position = 'relative';
+        element.style.width = cache_width;
+
+        document.body.classList.remove('print-preview');
     },
-    render: function () {
+render: function () {
         return (
             <div className="sidebar">
                 <Tabs>
