b6b9742314f5ae61c1936118875d592910b3f943
Michiel Renty <m.renty@gmail.com>
2016-09-27 22:12:19 +0200

    Converted Expire to ES6 Classes

--- readme.md
+++ readme.md
@@ -9,7 +9,7 @@
 
 `gulp`
 
-## Browsers support
+## Browser support
 
 | [<img src="https://raw.githubusercontent.com/godban/browsers-support-badges/master/src/images/firefox.png" alt="Firefox" width="16px" height="16px" />](http://godban.github.io/browsers-support-badges/)</br>Firefox | [<img src="https://raw.githubusercontent.com/godban/browsers-support-badges/master/src/images/chrome.png" alt="Chrome" width="16px" height="16px" />](http://godban.github.io/browsers-support-badges/)</br>Chrome | [<img src="https://raw.githubusercontent.com/godban/browsers-support-badges/master/src/images/opera.png" alt="Opera" width="16px" height="16px" />](http://godban.github.io/browsers-support-badges/)</br>Opera |
 | --------- | --------- | --------- |
--- scripts/components/expire.js
+++ scripts/components/expire.js
@@ -5,39 +5,49 @@
 
 import React from 'react';
 
-var Expire = React.createClass({
-    getDefaultProps: function () {
-        return {
-            delay: 1000,
-            type: 'info'
-        };
-    },
-    getInitialState: function () {
-        return {visible: true};
-    },
-    componentWillReceiveProps: function (nextProps) {
+class Expire extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = { visible: true };
+        this.setTimer = this.setTimer.bind(this);
+    }
+    componentWillReceiveProps(nextProps) {
         if (nextProps.children !== this.props.children) {
             this.setTimer();
             this.setState({visible: true});
         }
-    },
-    componentDidMount: function () {
+    }
+    componentDidMount() {
         this.setTimer();
-    },
-    setTimer: function () {
+    }
+    setTimer() {
         this._timer != null ? clearTimeout(this._timer) : null;
 
         this._timer = setTimeout(function(){
             this.setState({visible: false});
             this._timer = null;
         }.bind(this), this.props.delay);
-    },
-    render: function(){
+    }
+    render(){
         const className = 'form__message form__message--' + this.props.type;
         return this.state.visible
             ? <div className={className}>{this.props.children}</div>
             : <span />;
     }
-});
+}
+
+Expire.propTypes = {
+
+    delay: React.PropTypes.number,
+    type: React.PropTypes.string
+
+};
+
+Expire.defaultProps = {
+
+    delay: 1000,
+    type: 'info'
+
+};
 
 export default Expire;
